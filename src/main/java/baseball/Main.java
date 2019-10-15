eab63573ba66d94f95c5392680fe05e5edb9b45f
ydj515 <ydj515@hanmail.net>
2019-10-15 20:45:26 +0900

    Main.java

---
+++ src/main/java/baseball/Main.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseBallGameController baseBallGameController = new BaseBallGameController();
+		baseBallGameController.run();
+	}
+}
