98ede0862de5c78c47eee323efe1060685e86ffa
Kyeongukna <42959666+Kyeongukna@users.noreply.github.com>
2019-10-26 23:04:35 +0900

    [야구게임] 나경욱 미션 제출합니다. (#5)
    * README file
    
    * Baseball game controller commit
    
    * Baseball game Inputview commit
    
    * Baseball game output view commit
    
    * Baseball game main commit
    
    * Baseball game model commit
    
    * README Update
    
    * UserNumber delete
    
    * UserNumber plus
    
    * ViewGame은 사용자에게 게임상황을 보여주고 다음 게임을 진행할지 입력받습니다.
    InputUserNumber은 사용자의 숫자를 입력받는 역할을 해줍니다.
    
    * GameManager 의 변동에 따라 변경
    
    * UserNumber delete
    
    * plus UserNumber and logic
    
    * InputData -> Input data for user
    ViewGame -> Gameplay print
    
    * Main commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import controller.GameManager;
+
+public class Main {
+    public static void main(String[] args) {
+        GameManager play = new GameManager();
+        play.playBaseBallGame(1);
+    }
+}
---
+++ src/main/java/controller/GameManager.java
@@ -0,0 +1,56 @@
+package controller;
+
+import model.BaseBall;
+import view.InputUserNumber;
+import view.ViewGame;
+
+public class GameManager {
+    private final int ALL_STRIKE = 3;
+    private BaseBall BaseBall;
+    private InputUserNumber scanNumber;
+    private ViewGame viewGame;
+
+    public GameManager() {
+        BaseBall = new BaseBall();
+        scanNumber = new InputUserNumber();
+        viewGame = new ViewGame();
+    }
+
+    private boolean challengeOnce() {
+        scanNumber.oneGamePrint();
+        BaseBall.initUserNumber(scanNumber.inputUserBall());
+        printStatus();
+        if (BaseBall.countStrike() == ALL_STRIKE) return false;
+        return true;
+    }
+
+    private void printStatus() {
+        if (BaseBall.countStrike() == 0 && BaseBall.countBall() == 0) {
+            viewGame.nothingPrint();
+            return;
+        }
+        viewGame.strikePrint(BaseBall.countStrike());
+        viewGame.ballPrint(BaseBall.countBall());
+    }
+
+    private void nextGame() {
+        int next = viewGame.nextGameNumber();
+        if (next == 1 || next == 2) {
+            playBaseBallGame(next);
+        }
+        viewGame.closeNumberError();
+        nextGame();
+    }
+
+    public void playBaseBallGame(int nextGameStatus) {
+        if (nextGameStatus == 2) {
+            System.exit(1);
+        }
+        BaseBall.resetComputerBall();
+        while (true) {
+            if (!challengeOnce()) break;
+        }
+        viewGame.winGamePrint();
+        nextGame();
+    }
+}
