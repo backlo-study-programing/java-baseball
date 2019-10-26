5a90407b2790c803fe46f585e972e812ba70d834
Kyeongukna <ruddnr96@naver.com>
2019-10-26 21:49:24 +0900

    UserNumber delete

--- src/main/java/controller/GameManager.java
+++ src/main/java/controller/GameManager.java
@@ -1,7 +1,6 @@
 package controller;
 
 import model.BaseBall;
-<<<<<<< HEAD
 import view.InputUserNumber;
 import view.ViewGame;
 
@@ -21,14 +20,14 @@ public class GameManager {
         scanNumber.oneGamePrint();
         BaseBall.initUserNumber(scanNumber.inputUserBall());
         printStatus();
-        if(BaseBall.countStrike() == ALL_STRIKE) return false;
+        if (BaseBall.countStrike() == ALL_STRIKE) return false;
         return true;
     }
 
     private void printStatus() {
-        if(BaseBall.countStrike() == 0 && BaseBall.countBall() == 0) {
+        if (BaseBall.countStrike() == 0 && BaseBall.countBall() == 0) {
             viewGame.nothingPrint();
-            return ;
+            return;
         }
         viewGame.strikePrint(BaseBall.countStrike());
         viewGame.ballPrint(BaseBall.countBall());
@@ -44,96 +43,14 @@ public class GameManager {
     }
 
     public void playBaseBallGame(int nextGameStatus) {
-        if(nextGameStatus == 2) {
+        if (nextGameStatus == 2) {
             System.exit(1);
         }
         BaseBall.resetComputerBall();
         while (true) {
-            if(!challengeOnce()) break;
-=======
-import view.InputData;
-import view.ViewGame;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-
-public class GameManager {
-    private BaseBall computer = new BaseBall();
-    private InputData scanData = new InputData();
-    private ViewGame viewGame = new ViewGame();
-    private List<Integer> userNumber = new ArrayList<>();
-    private int strike;
-    private int ball;
-    public GameManager() {
-        oneGamePlay();
-    }
-
-    private boolean challengeOnce() {
-        userNumber.clear();
-        viewGame.oneGamePrint();
-        setUserNumber(scanData.inputUserBall());
-        countBallAndStrike();
-        printStatus();
-        if(strike == 3) return false;
-        return true;
-    }
-
-    private void printStatus(){
-        if(strike == 0 && ball == 0) {
-            viewGame.nothingPrint();
-            return ;
-        }
-        viewGame.strikePrint(strike);
-        viewGame.ballPrint(ball);
-    }
-    private void countBallAndStrike() {
-        this.ball= countBall();
-        this.strike = countStrike();
-    }
-    private void setUserNumber(String userBall) {
-        for(int i = 0 ; i<userBall.length() ; i++) {
-            userNumber.add(Integer.parseInt(userBall.split("")[i]));
-        }
-    }
-
-    private void oneGamePlay() {
-        computer.resetAllBall();
-        while (challengeOnce()) {
->>>>>>> a0ac3cff4d92c4385ea582458fe06a2b05c405fb
+            if (!challengeOnce()) break;
         }
         viewGame.winGamePrint();
         nextGame();
     }
-<<<<<<< HEAD
 }
-=======
-
-    private void nextGame() {
-        int next = scanData.nextGameNuber();
-        switch (next) {
-            case 1 : oneGamePlay();
-                break;
-            case 2 : System.exit(1);
-                break;
-            default : viewGame.closeNumberError();
-                nextGame();
-                break;
-        }
-    }
-
-    private int countBall() {
-        return IntStream.range(0,userNumber.size())
-                        .map(i -> (computer.getBalls().get(i) == userNumber.get((i + 1) % 3)) || (computer.getBalls().get(i) == userNumber.get((i + 2) % 3)) ? 1:0)
-                        .sum();
-    }
-
-    private int countStrike() {
-        return IntStream.range(0,userNumber.size())
-                .map(i -> (computer.getBalls().get(i) == userNumber.get(i)) ? 1:0)
-                .sum();
-    }
-
-
-}
->>>>>>> a0ac3cff4d92c4385ea582458fe06a2b05c405fb
