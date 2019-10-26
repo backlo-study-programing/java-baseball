2cfbe3b8ef56a561f0594395b14a4eadf3151298
Kyeongukna <ruddnr96@naver.com>
2019-10-26 21:52:08 +0900

    Main commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,6 @@ import controller.GameManager;
 public class Main {
     public static void main(String[] args) {
         GameManager play = new GameManager();
-<<<<<<< HEAD
         play.playBaseBallGame(1);
     }
 }
-=======
-    }
-}
->>>>>>> a0ac3cff4d92c4385ea582458fe06a2b05c405fb
