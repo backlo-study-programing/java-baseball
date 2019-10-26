67db540f52e7d2f350fe04f79237be782232466b
Kyeongukna <ruddnr96@naver.com>
2019-10-26 21:49:56 +0900

    plus UserNumber and logic

--- src/main/java/model/BaseBall.java
+++ src/main/java/model/BaseBall.java
@@ -4,15 +4,11 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
-<<<<<<< HEAD
 import java.util.stream.IntStream;
-=======
->>>>>>> a0ac3cff4d92c4385ea582458fe06a2b05c405fb
 
 public class BaseBall {
     private final int MAX_BALL_COUNT = 3;
     private final int MAX_RANDOM_NUMBER = 9;
-<<<<<<< HEAD
     private List<Integer> computerNumber;
     private List<Integer> userNumber;
 
@@ -32,12 +28,6 @@ public class BaseBall {
         computerNumber.add(getRandom());
         HashSet<Integer> newBall = new HashSet<Integer>(computerNumber);
         computerNumber = new ArrayList<>(newBall);
-=======
-    private List<Integer> balls = new ArrayList<>();
-
-    public BaseBall() {
-        setAllBalls();
->>>>>>> a0ac3cff4d92c4385ea582458fe06a2b05c405fb
     }
 
     private int getRandom() {
@@ -45,7 +35,6 @@ public class BaseBall {
         return (random + 1);
     }
 
-<<<<<<< HEAD
     public void resetComputerBall() {
         computerNumber.clear();
         initComputerBalls();
@@ -72,26 +61,5 @@ public class BaseBall {
 
     public List<Integer> getComputerNumber() {
         return this.computerNumber;
-=======
-    private void setBall() {
-        balls.add(getRandom());
-        HashSet<Integer> tmpball = new HashSet<Integer>(balls);
-        balls = new ArrayList<>(tmpball);
-    }
-
-    private void setAllBalls() {
-        while (balls.size() != 3){
-            setBall();
-        }
-    }
-
-    public void resetAllBall() {
-        balls.clear();
-        setAllBalls();
-    }
-
-    public List<Integer> getBalls() {
-        return this.balls;
->>>>>>> a0ac3cff4d92c4385ea582458fe06a2b05c405fb
     }
 }
